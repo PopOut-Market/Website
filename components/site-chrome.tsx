8fb0f113a2d330128895c0132f559788af9c66b3
Ericcccccc777 <haizhou0807@gmail.com>
2026-04-28 16:17:55 +1000

    Revert footer FAQ label test change

--- components/site-chrome.tsx
+++ components/site-chrome.tsx
@@ -140,9 +140,9 @@ function footerFaqLabel(locale: Locale): string {
   switch (locale) {
     case "zh-Hans":
     case "zh-Hant":
-      return "FAQ2";
+      return "FAQ";
     default:
-      return "FAQ2";
+      return "FAQ";
   }
 }
 
